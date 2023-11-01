e34201326e837a3d3b1d447ecbad4eef2c90c2b3
Simone <144659113+Simonesho@users.noreply.github.com>
2023-11-01 03:29:35 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,54 @@
+public class RandomDataGenerator {
+        int[] data = new int[10];
+    }
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        }
+}
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        }        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+
+def main():
+
+    main()
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+def generate_random_data():
+if __name__ == "__main__":
+import random
+    return data;
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+    });
+
+
+        console.log("Random Number: " + item);
+    const data = [];
+    }
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+function main() {
+}
+    const data = generateRandomData();
+}
+    for item in data:
+if __name__ == "__main__":
+import random
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    return data
+    data = generate_random_data()
+    main()
+
+def main():
+
